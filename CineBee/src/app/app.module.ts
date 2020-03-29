49bdebe72e238ad618efafa82dbb76fcdeddfaf3
Tejaswini2403 <58465387+Tejaswini2403@users.noreply.github.com>
2020-03-30 02:32:47 +0530

    Commit4

--- CineBee/src/app/app.module.ts
+++ CineBee/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxIconModule } from 'igniteui-angular';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { FooterComponent } from './footer/footer.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    IgxIconModule,
+    MatIconModule,
+    MatMenuModule,
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
--- CineBee/src/app/header/header.component.ts
+++ CineBee/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { IgxIconService } from "igniteui-angular";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private iconService: IgxIconService) { }
   ngOnInit(): void {
+    //creating icon with svg
+    this.iconService.addSvgIcon("menu", "../../assets/images/svg/menu.svg", "filter-icons");
   }
-
 }
