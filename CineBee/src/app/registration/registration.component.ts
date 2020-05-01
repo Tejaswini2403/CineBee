9722c09abf200691cd912400d2e0a2b021ef593f
Tejaswini2403 <58465387+Tejaswini2403@users.noreply.github.com>
2020-05-01 20:38:15 +0530

    Commit 17

--- CineBee/src/app/registration/registration.component.ts
+++ CineBee/src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControlName } from '@angular/forms';
 import { Router } from '@angular/router';
 import {UserService} from '../user.service';
 
@@ -15,12 +15,13 @@ export class RegistrationComponent implements OnInit {
 
   registerForm: FormGroup;
     submitted = false;
+    a:Number;
+    b:Number;
 
     constructor(private formBuilder: FormBuilder,private router:Router, private user:UserService) { }
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
-            title: ['', Validators.required],
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
@@ -39,11 +40,15 @@ export class RegistrationComponent implements OnInit {
         this.submitted = true;
 
         // stop here if form is invalid
-        if(!this.registerForm.invalid){
+        this.a=this.registerForm.value.email.indexOf("@");
+        this.b=this.registerForm.value.email.indexOf(".com");
+        if(this.registerForm.value.firstName==""||this.registerForm.value.lastName==""
+            ||this.registerForm.value.email==""||this.registerForm.value.password==""||this.registerForm.value.confirmPassword==""
+            ||this.registerForm.value.password!=this.registerForm.value.confirmPassword||this.a==-1||this.b==-1||
+            this.registerForm.value.acceptTerms==false)
             return;
-        }
-        // display form values on success
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+        else
+            this.router.navigate(['homepage']);
     }
 
     onReset() {
